5fee037ed0cc9a6c7e2fc80876c99f73cda37510
stefanbanu <stefanbanu1@gmail.com>
2015-01-01 22:03:52 +0200

    Added the heart of the game, the game loop.

--- JavaTileGame/src/com/stefanbanu/tile/Game.java
+++ JavaTileGame/src/com/stefanbanu/tile/Game.java
@@ -2,16 +2,62 @@ package com.stefanbanu.tile;
 
 import com.stefanbanu.tile.display.Display;
 
-public class Game {
+public class Game implements Runnable{
 
 	private Display display;
+	private Thread thread;
+	
+	private boolean running = false;
 	
 	public int width, height;
+	private String title;
 	
 	public Game(String title, int width, int height) {
 		this.width = width;
 		this.height = height;
+		this.title = title;
 		
+		start();
+	}
+
+	
+	@Override
+	public void run() {
+		init();	
+		
+		while(running){
+			update();
+			render();
+		}
+		// just in case is not already stopped
+		stop();
+	}
+	private void init() {
 		display = new Display(title, width, height);
+	}
+	private void update() {
+	//	System.out.println("running");
+	}
+	private void render() {
+		
+	}
+
+	public synchronized void start(){
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
