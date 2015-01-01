8c1a7f9a394b39719b121c6424953424f176f12c
stefanbanu <stefanbanu1@gmail.com>
2015-01-01 21:03:49 +0200

    Master commit.

---
+++ JavaTileGame/src/com/stefanbanu/tile/MainClass.java
@@ -0,0 +1,12 @@
+package com.stefanbanu.tile;
+
+import com.stefanbanu.tile.display.Display;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		new Display("Title!", 300, 300);
+
+	}
+
+}
